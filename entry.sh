72a84b452d78fa5e60db1ec948029e3a5c339a01
Geeku <ggongpei@qq.com>
2016-08-07 14:08:26 +0800

    rm validate from entry.sh

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@
   +force_install_dir /root/dst/ \
   +login anonymous \
   +app_update 343050 \
-  validate +quit &&
+  +quit &&
 /root/dst/bin/dontstarve_dedicated_server_nullrenderer \
   persistent_storage_root \
   /root/.klei "$@"
