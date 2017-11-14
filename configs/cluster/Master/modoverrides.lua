295c89172283fb32ac724b14797f3692e5531791
Geeku <ggongpei@gmail.com>
2017-11-14 16:10:37 +0800

    my config

--- configs/cluster/Master/modoverrides.lua
+++ configs/cluster/Master/modoverrides.lua
@@ -1,23 +1,8 @@
 return {
-  ["workshop-347079953"]={
-    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
-    enabled=true 
-  },
   ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
-  ["workshop-375859599"]={
-    configuration_options={
-      divider=5,
-      random_health_value=0,
-      random_range=0,
-      send_unknwon_prefabs=true,
-      show_type=0,
-      unknwon_prefabs=1,
-      use_blacklist=true 
-    },
-    enabled=true 
-  },
   ["workshop-378160973"]={
     configuration_options={
+      ENABLEPINGS=true,
       FIREOPTIONS=2,
       OVERRIDEMODE=false,
       SHAREMINIMAPPROGRESS=true,
@@ -27,5 +12,14 @@ return {
     },
     enabled=true 
   },
-  ["workshop-462469447"]={ configuration_options={  }, enabled=true } 
+  ["workshop-666155465"]={
+    configuration_options={
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1 
+    },
+    enabled=true 
+  } 
 }
--- configs/mods/modsettings.lua
+++ configs/mods/modsettings.lua
@@ -1,17 +1,3 @@
--- Use the "ForceEnableMod" function when developing a mod. This will cause the
--- game to load the mod every time no matter what, saving you the trouble of
--- re-enabling it from the main menu.
---
--- Note! You shout NOT do this for normal mod loading. Please use the Mods menu
--- from the main screen instead.
-
---ForceEnableMod("kioskmode_dst")
-
-
--- Use "EnableModDebugPrint()" to show extra information during startup.
-
---EnableModDebugPrint()
-
--- Use "EnableModError()" to make the game more strict and crash on bad mod practices.
-
---EnableModError()
+ForceEnableMod("362175979")
+ForceEnableMod("378160973")
+ForceEnableMod("378160973")
