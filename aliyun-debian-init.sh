1875dd5e3162d619d5b97dfbcaf9b2cd0416dd60
Geeku <ggongpei@gmail.com>
2017-11-14 16:24:49 +0800

    docker stable

--- aliyun-debian-init.sh
+++ aliyun-debian-init.sh
@@ -14,7 +14,7 @@ deb-src http://mirrors.163.com/debian-security/ jessie/updates main non-free con
 EOF
 
 apt-get update
-curl -sSL https://get.daocloud.io/docker-test | sh
+curl -sSL https://get.daocloud.io/docker | sh
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
