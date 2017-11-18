1c43b75ced956a7bac99fa94e26ad4bd563a5546
Geeku <ggongpei@gmail.com>
2017-11-18 10:52:55 +0800

    update

--- aliyun-init.sh
+++ aliyun-init.sh
@@ -9,15 +9,15 @@ if [ -n "$apt" ]; then
 	# aliyun debian source is broken
 	mv /etc/apt/sources.list.d/sources-aliyun-0.list /etc/apt/sources.list.d/sources-aliyun-0.list.bak
 	tee /etc/apt/sources.list << EOF
-	deb http://mirrors.163.com/debian/ jessie main non-free contrib
-	deb http://mirrors.163.com/debian/ jessie-updates main non-free contrib
-	deb http://mirrors.163.com/debian/ jessie-backports main non-free contrib
-	deb-src http://mirrors.163.com/debian/ jessie main non-free contrib
-	deb-src http://mirrors.163.com/debian/ jessie-updates main non-free contrib
-	deb-src http://mirrors.163.com/debian/ jessie-backports main non-free contrib
-	deb http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
-	deb-src http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
-	EOF
+deb http://mirrors.163.com/debian/ jessie main non-free contrib
+deb http://mirrors.163.com/debian/ jessie-updates main non-free contrib
+deb http://mirrors.163.com/debian/ jessie-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ jessie main non-free contrib
+deb-src http://mirrors.163.com/debian/ jessie-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ jessie-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
+EOF
     apt-get update
     apt-get -y install $package
 elif [ -n "$yum" ]; then
